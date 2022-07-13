84c7f4b010dd7bbc255b7e1a90868166f6c0d69d
danushka181 <danushka181@gmail.com>
2022-07-13 23:48:50 +0530

    customers page development

--- lib/services/globals.dart
+++ lib/services/globals.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
-const String base_url = "http://10.0.2.2:8000/api/";
+// const String base_url = "http://10.0.2.2:8000/api/";
 // const String base_url = "https://reqres.in/api/";
-// const String base_url = "http://aaf3-2402-4000-2380-c5a6-8b2-d08d-4cef-d817.ngrok.io/api/";
+const String base_url = "https://cb7f-2402-4000-2380-c5a6-cd4a-12ca-8103-ea07.ngrok.io/api/";
 
 
 const Map<String, String> headers = {"Content-Type": "Application/json"};
